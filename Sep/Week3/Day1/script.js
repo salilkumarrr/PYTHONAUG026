04cce21e1e3e5829dacb4a4451c8b02b8d180910
salilkumarrr <salilkumarpolai9@gmail.com>
2026-09-16 20:28:10 +0530

    week3day1

---
+++ Sep/Week3/Day1/script.js
@@ -0,0 +1,22 @@
+// && // !
+console.log(3>2 && 1=="1");
+
+let a = 4;
+console.log(3>2 || a++ <5);
+console.log(a);
+
+console.log(!true);
+console.log(!false);
+
+console.log(!"");
+console.log(!0);
+
+//special functionalities of + operator
+
+//addition
+//concatnation
+//typecasting
+
+console.log(2+3);
+console.log(typeof(2+"5"));
+console.log(4+"5"+5);
