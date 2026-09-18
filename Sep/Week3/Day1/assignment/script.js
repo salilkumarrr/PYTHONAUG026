00fb0e2899f6ee6a190cd4c86b648da0ea9ca696
salilkumarrr <salilkumarpolai9@gmail.com>
2026-09-18 17:19:24 +0530

    loop

---
+++ Sep/Week3/Day1/assignment/script.js
@@ -0,0 +1,22 @@
+let stdScore = 85;
+
+switch (true) {
+  case stdScore >= 90:
+    console.log("A");
+    break;
+
+  case stdScore >= 80:
+    console.log("B");
+    break;
+
+  case stdScore >= 70:
+    console.log("C");
+    break;
+
+  case stdScore >= 40:
+    console.log("D");
+    break;
+
+  default:
+    console.log("F");
+}
